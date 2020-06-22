832a6185293f4c4b2a3a1842ba66a3fcfafc2044
Micah Jeng <FXM367@motorolasolutions.com>
2020-06-21 20:36:16 -0500

    added initial template

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Start up the containers
+docker-compose up -d
+# Check that containers are up and running
+docker-compose ps
---
+++ web/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from elasticsearch import Elasticsearch
+
+es_host = os.environ['ELASTICSEARCH_URL']
+print('Elastic host is {}'.format(es_host))
+es = Elasticsearch([es_host])
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return "<h1>My Flask Application</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
